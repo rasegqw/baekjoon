e5e7874818d4fa60f6e58792cdde10be55f3f7bd
rasegqw <yunsuk224@gmail.com>
2025-03-20 10:54:13 +0900

    [Silver II] Title: 부분수열의 합, Time: 3384 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,15 @@
-N, S = map(int, input().split())  # 정수 개수 N, 목표 합 S 입력
-num = list(map(int, input().split()))  # 수열 입력
+N, S = map(int, input().split())
 
+Nums = list(map(int, input().split()))
 count = 0
 
 for bit in range(1, 1<<N):
-    sum_part = 0
+    sum = 0
     for i in range(N):
         if bit & (1<<i):
-            sum_part += num[i]
-    
-    if sum_part == S :
-        count +=1
-
+            sum += Nums[i]
+    if sum == S:
+        count += 1
+            
 
 print(count)
