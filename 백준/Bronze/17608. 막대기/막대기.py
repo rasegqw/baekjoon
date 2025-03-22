356775796adf17b845fbf5cd2cace5fb08515cd9
rasegqw <yunsuk224@gmail.com>
2025-03-22 10:57:12 +0900

    [Bronze II] Title: 막대기, Time: 96 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -3,18 +3,17 @@ import sys
 N = int(input())
 
 stack = [None] * N
-DP = [1] * N
 
 for i in range(N):
     stack[i] = int(sys.stdin.readline())
 
 pivot_num = stack[-1]
+count = 1
+while len(stack) != 0:
+    a = stack.pop()
+    if a>pivot_num :
+        pivot_num = a
+        count += 1
+    
 
-for i in range(N-2, -1, -1):
-    if stack[i] > pivot_num:
-        DP[i] = DP[i+1] + 1
-        pivot_num = stack[i]
-    else:
-        DP[i] = DP[i+1]
-
-print(DP[0])
+print(count)
