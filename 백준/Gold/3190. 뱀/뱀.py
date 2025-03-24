5673e73e9fe7107167dc939a709854a8f13ff3b0
rasegqw <yunsuk224@gmail.com>
2025-03-24 13:47:34 +0900

    [Gold IV] Title: 뱀, Time: 168 ms, Memory: 114772 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -0,0 +1,65 @@
+import sys
+from collections import deque
+import copy
+input = sys.stdin.readline
+
+N = int(input())
+
+Anum = int(input())
+Apple = [0] * Anum
+
+for i in range(Anum):
+    Apple[i] = list(map(int, input().split()))
+    Apple[i][0] -= 1
+    Apple[i][1] -= 1
+
+Lcount = int(input())
+Ltime = []
+Ldirec = deque()
+for i in range(Lcount):
+    time, direc = map(str, input().split())
+    time= int(time)
+    Ltime.append(time)
+    Ldirec.append(direc)
+
+Timecount = 0
+
+snake = [[0, 0]]
+vec = [0, 1]  # [y, x]
+isGood = True
+
+while isGood:
+    Timecount += 1
+    new_head = []
+
+    # vec 방향에 따라 진행
+
+    new_head.append(snake[0][0] + vec[0])
+    new_head.append(snake[0][1] + vec[1])
+    snake.insert(0, new_head)
+    
+
+    # 진행한 공간이 벽인지 판단
+    if new_head[0] >= N or new_head[0] < 0 or new_head[1] >= N or new_head[1] < 0:
+        break
+
+    # 진행한 공간에 자기 자신이 있는지 판단
+    if new_head in snake[1:]:
+        break
+
+    # 진행한 공간에 사과가 있는지 판단
+    if snake[0] in Apple:
+        Apple.remove(snake[0])
+    else:
+        snake.pop()
+
+    # 방향을 바꿀것인지 판단
+    if Timecount in Ltime:
+        if Ldirec.popleft() == 'D':
+            vec = [vec[1], -vec[0]]
+
+        else:
+            vec = [-vec[1], vec[0]]
+
+
+print(Timecount)
