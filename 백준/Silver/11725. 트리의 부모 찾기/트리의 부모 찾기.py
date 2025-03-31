b27debc97731b9e5eb396bf68ddc1e8d811d155d
rasegqw <yunsuk224@gmail.com>
2025-03-31 20:26:57 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 232 ms, Memory: 139192 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -5,24 +5,28 @@ input = sys.stdin.readline
 
 N = int(input())
 
-tree = {i: [] for i in range(1, N + 1)}
+Nodes = [[] for _ in range(N+1)]
 
-for _ in range(N - 1):
-    a, b = map(int, input().split())
-    tree[a].append(b)
-    tree[b].append(a)
+for i in range(1, N):
+    a, b = map(int, input().strip().split())
 
-parent = [0] * (N + 1)  
-queue = deque([1])  
-visited = set([1])  
+    Nodes[a].append(b)
+    Nodes[b].append(a)
 
-while queue:
-    node = queue.popleft()
-    for child in tree[node]:  # 현재 노드의 연결된 노드들 탐색
-        if child not in visited:  # 방문하지 않은 노드만 처리
-            parent[child] = node  # 현재 노드가 자식 노드의 부모가 됨
-            visited.add(child)
-            queue.append(child)
+visited = set()
+parents = [i for i in range(N+1)]
+que = deque([1])
 
-for i in range(2, N + 1):
-    print(parent[i])
+while que:
+    x = que.popleft()
+
+    for i in Nodes[x]:
+        if i in visited:
+            continue
+        parents[i] = x
+        que.append(i)
+
+    visited.add(x)
+
+for i in range(2, N+1):
+    print(parents[i])
