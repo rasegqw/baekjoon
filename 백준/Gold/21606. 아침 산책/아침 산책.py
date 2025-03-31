2c424b6ce042a6891d0fae0899c882d54d769643
rasegqw <yunsuk224@gmail.com>
2025-03-31 13:44:41 +0900

    [Gold III] Title: 아침 산책, Time: 368 ms, Memory: 327716 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,41 +1,52 @@
 import sys
 sys.setrecursionlimit(10**5)
-input = sys.stdin.readline
+input = sys.stdin.read
 
-N = int(input())
-a = input().strip()
-place = [0] + list(map(int, a))  # 1-based index
+def dfs(node):
+    stack = [node]
+    visited.add(node)
+    count = 0  # 실내 노드 개수
 
-graph = [[] for _ in range(N + 1)]
-for _ in range(N - 1):
-    u, v = map(int, input().split())
+    while stack:
+        cur = stack.pop()
+        for neighbor in graph[cur]:
+            if neighbor in visited:
+                continue
+            if indoor[neighbor - 1] == 1:  # 실내 노드라면 카운트 증가
+                count += 1
+            else:  # 실외 노드라면 계속 탐색
+                visited.add(neighbor)
+                stack.append(neighbor)
+    
+    return count
+
+# 입력 처리
+data = input().split()
+n = int(data[0])
+indoor = list(map(int, list(data[1][:n])))  # 실내(1), 실외(0) 정보
+edges = list(map(int, data[2:]))  # 간선 정보
+
+# 그래프 구성
+graph = [[] for _ in range(n + 1)]
+for i in range(0, len(edges), 2):
+    u, v = edges[i], edges[i + 1]
     graph[u].append(v)
     graph[v].append(u)
 
-visited = [False] * (N + 1)
-answer = 0
+# 1. 실내(1) 노드끼리 직접 연결된 경우 카운트
+result = 0
+for i in range(1, n + 1):
+    if indoor[i - 1] == 1:  # 실내 노드라면
+        for neighbor in graph[i]:
+            if indoor[neighbor - 1] == 1:  # 실내-실내 연결
+                result += 1
+# result //= 2  # 중복 계산 제거
 
-def dfs(node):
-    visited[node] = True
-    indoor_count = 0  # 실내 노드 개수 카운트
-
-    for neighbor in graph[node]:
-        if place[neighbor] == 1:  # 연결된 실내 노드 찾기
-            indoor_count += 1
-        elif not visited[neighbor]:  # 실외 노드라면 DFS 계속 탐색
-            indoor_count += dfs(neighbor)
-
-    return indoor_count
-
-for i in range(1, N + 1):
-    if place[i] == 0 and not visited[i]:  # 실외 노드에서 탐색
+# 2. 실외(0) 노드를 거치는 경우 계산
+visited = set()
+for i in range(1, n + 1):
+    if indoor[i - 1] == 0 and i not in visited:  # 실외 노드면서 방문하지 않은 경우
         cnt = dfs(i)
-        answer += cnt * (cnt - 1)  # 가능한 모든 (i, j) 경우의 수 추가
+        result += cnt * (cnt - 1)  # 두 노드 선택 방법
 
-for i in range(1, N + 1):
-    if place[i] == 1:
-        for j in graph[i]:
-            if place[j] == 1:  # 실내-실내 직접 연결된 경우
-                answer += 1  # 중복 방지 X (모든 간선 체크 필요)
-
-print(answer)
+print(result)
