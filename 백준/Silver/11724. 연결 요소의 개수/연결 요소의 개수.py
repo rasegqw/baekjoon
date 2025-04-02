f26b875525b2406452c412d02f63cca1f031091f
rasegqw <yunsuk224@gmail.com>
2025-04-02 19:10:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 312 ms, Memory: 110900 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,32 +1,26 @@
-N, M =map(int, input().split())
+# 연결 요소의 개수
 
-parent = [i for i in range(N+1)]
+N, M = map(int, input().split())
 
+parents= [i for i in range(N+1)]
 
 def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+    if parents[x] != x:
+        x = find(parents[x])
+    return x
 
-
-def union(x, y):
-    root_x = find(x)
-    root_y = find(y)
-
-    if root_x != root_y:
-        parent[root_y] = root_x
-        for i in range(N+1):
-            if parent[i] == root_y:
-                parent[i] = root_x
-
-
+def union(a, b):
+    root_a = find(a)
+    root_b = find(b)
+    if root_a != root_b:
+        for i in range(1, N+1):
+            if parents[i] == root_b:
+                parents[i] = root_a
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    union(a, b)
+    u, v = map(int, input().split())
+    union(u, v)
 
+res = set(parents)
 
-parent = set(parent)
-
-
-print(len(parent)-1)
+print(len(res)-1)
