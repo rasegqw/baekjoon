21284fe556f847877d9ed6c2ef800a1db86fc960
rasegqw <yunsuk224@gmail.com>
2025-04-06 16:12:31 +0900

    [Gold V] Title: 회의실 배정, Time: 2716 ms, Memory: 47528 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+schedule = []
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    schedule.append([end, start])
+
+schedule.sort()
+
+count = 0
+now_start = 0
+now_end = 0
+for end, start in schedule:
+    if start >= now_end:
+        now_start = start
+        now_end = end
+        count += 1
+    
+print(count)
