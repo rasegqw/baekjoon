625523783f451195d21e092bedde671a9861294e
rasegqw <yunsuk224@gmail.com>
2025-04-06 15:39:39 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,16 @@
+x = list(map(str, input().split('-')))
+
+nums = []
+for i in x:
+    if i.find('+'):
+        plus = list(map(int, i.split('+')))
+        nums.append(sum(plus))
+    else:
+        nums.append(int(i))
+
+
+result = nums[0]
+for i in range(1, len(nums)):
+    result -= nums[i]
+
+print(result)
