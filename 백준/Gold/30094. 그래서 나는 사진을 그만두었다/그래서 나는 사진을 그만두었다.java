ed87ae111d77b2764db5b4653cc54c58d8cf0fdd
rasegqw <yunsuk224@gmail.com>
2025-10-30 13:59:11 +0900

    [Gold II] Title: 그래서 나는 사진을 그만두었다, Time: 908 ms, Memory: 74424 KB -BaekjoonHub

--- 백준/Gold/30094. 그래서 나는 사진을 그만두었다/그래서 나는 사진을 그만두었다.java
+++ 백준/Gold/30094. 그래서 나는 사진을 그만두었다/그래서 나는 사진을 그만두었다.java
@@ -2,100 +2,106 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final long MOD = 998244353L;
 
-    static class Student {
-        int idx;     // 학생 번호 (출력용)
-        long c, a;   // 입력값
-        long v;      // c - a
+  static int DIV_NUM = 998244353;
+  static int N;
+  static long[][] totalDiff;
+  static long maxRes = 0;
+  static long constant;
+  static long minRes = Long.MAX_VALUE;
+  
 
-        Student(int idx, long c, long a) {
-            this.idx = idx;
-            this.c = c;
-            this.a = a;
-            this.v = c - a;
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    totalDiff = new long[N][2];
+    Map<Long, Integer> diffMap = new HashMap<>();
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+
+      long c = Integer.parseInt(st.nextToken());
+      long a = Integer.parseInt(st.nextToken());
+      totalDiff[i][0] = c - a;
+      totalDiff[i][1] = i;
+      if (diffMap.containsKey(totalDiff[i][0])) {
+        int x = diffMap.get(totalDiff[i][0]);
+        diffMap.put(totalDiff[i][0], x+1);
+      } else {
+        diffMap.put(totalDiff[i][0], 1);
+      }
+
+      constant -= c;
+      constant += a * N;
     }
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    // 학생 i의 인물 점수 : c_i*l_i + a_i*r_i
+    // l_i : 학생i 왼쪽 사람 수, r_i : 오른쪽 사람 수
+    // 사진 점수 : N명의 인물점수 모두 더하기.
 
-        int N = Integer.parseInt(br.readLine());
-        Student[] arr = new Student[N];
+    // sum(c*(i-1) + a*(N-i)) 이 값을 구하고 싶은 거니까,
+    // sum(i*(c-a)-c+a*N)으로 식을 고칠 수 있었고,
+    // sum(-c + a*N) 부분은 상수이기 때문에, sum(i*(c-a)) 만 신경쓰면 됐다.
+    
+    // 결국 전체 팩토리얼을 브루트포스로 만들어낼 필요는 없었고, c-a 값이 같은 놈들 집합이 몇개, 몇개의 원소를 가지고 있는지만 파악해서, 경우의 수를 계산하면 됐다.
+    // 그래서 최댓값을 구하기 위해선, c-a를 오름차순으로 배치하면 되고,
+    // 최솟값을 구하기 위해선 c-a를 내림차순으로 배치하면 된다.
+    Arrays.sort(totalDiff, (a, b) -> Long.compare(b[0], a[0]));
+    long res = 0;
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            long c = Long.parseLong(st.nextToken());
-            long a = Long.parseLong(st.nextToken());
-            arr[i] = new Student(i + 1, c, a);
-        }
-
-        // --- 최소값 계산 (v 내림차순 정렬)
-        Student[] minArr = arr.clone();
-        Arrays.sort(minArr, (o1, o2) -> Long.compare(o2.v, o1.v));
-        long minScore = calcScore(minArr);
-        long minWays = countWays(minArr);
-        String minOrder = getOrder(minArr);
-
-        // --- 최대값 계산 (v 오름차순 정렬)
-        Student[] maxArr = arr.clone();
-        Arrays.sort(maxArr, Comparator.comparingLong(o -> o.v));
-        long maxScore = calcScore(maxArr);
-        long maxWays = countWays(maxArr);
-        String maxOrder = getOrder(maxArr);
-
-        // --- 출력
-        StringBuilder sb = new StringBuilder();
-        sb.append(minScore).append(" ").append(minWays).append("\n");
-        sb.append(minOrder).append("\n");
-        sb.append(maxScore).append(" ").append(maxWays).append("\n");
-        sb.append(maxOrder);
-        System.out.println(sb);
+    for (int i = 0; i < N; i++) {
+        res += totalDiff[i][0] * (i+1);
     }
 
-    // 전체 점수 계산
-    static long calcScore(Student[] arr) {
-        int N = arr.length;
-        long sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += arr[i].c * i + arr[i].a * (N - 1 - i);
-        }
-        return sum;
+    long cnt = 1;
+    for (int val : diffMap.values()) {
+        // System.out.println("val : " + val);
+        cnt *= factorial(val);
+        cnt %= DIV_NUM;
+    }
+    System.out.println(res+constant + " " +cnt);
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i<N; i++) {
+        sb.append(totalDiff[i][1]+1 + " ");
+    }
+    System.out.println(sb.toString());
+
+    Arrays.sort(totalDiff, (a, b) -> Long.compare(a[0], b[0]));
+    res = 0;
+
+    for (int i = 0; i < N; i++) {
+        res += totalDiff[i][0] * (i+1);
     }
 
-    // 같은 v 값을 가진 학생끼리 팩토리얼 곱 (모듈러 적용)
-    static long countWays(Student[] arr) {
-        long result = 1;
-        int cnt = 1;
+    cnt = 1;
+    for (int val : diffMap.values()) {
+        cnt *= factorial(val);
+        cnt %= DIV_NUM;
+    }
+    System.out.println(res+constant + " " +cnt);
 
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i].v == arr[i - 1].v) cnt++;
-            else {
-                result = (result * factorial(cnt)) % MOD;
-                cnt = 1;
-            }
-        }
-        result = (result * factorial(cnt)) % MOD;
-        return result;
+    sb = new StringBuilder();
+
+    for (int i = 0; i<N; i++) {
+        sb.append(totalDiff[i][1]+1 + " ");
+    }
+    System.out.println(sb.toString());
+  }
+
+
+  public static long factorial(int n) {
+
+    long result = 1;
+
+    for (int i = 1 ; i <= n; i++) {
+      result *= i;
+      result %= DIV_NUM;
     }
 
-    // 팩토리얼 계산
-    static long factorial(int n) {
-        long f = 1;
-        for (int i = 2; i <= n; i++) {
-            f = (f * i) % MOD;
-        }
-        return f;
-    }
-
-    // 줄 서기 순서 문자열로 변환
-    static String getOrder(Student[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            if (i > 0) sb.append(" ");
-            sb.append(arr[i].idx);
-        }
-        return sb.toString();
-    }
+    return result;
+  }
 }
